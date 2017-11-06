0b61190c92ab800ff26839841e4644d178075337
Kory33 <6561358+kory33@users.noreply.github.com>
2017-11-07 02:23:27 +0900

    add FileUtil class

---
+++ src/main/kotlin/com/github/kory33/usercommandregistry/util/FileUtil.kt
@@ -0,0 +1,89 @@
+package com.github.kory33.usercommandregistry.util
+
+import com.google.gson.JsonObject
+import com.google.gson.JsonParser
+import java.io.File
+import java.io.IOException
+import java.nio.charset.Charset
+import java.nio.file.FileVisitResult
+import java.nio.file.Files
+import java.nio.file.Path
+import java.nio.file.SimpleFileVisitor
+import java.nio.file.attribute.BasicFileAttributes
+
+private val fileFormat = Charset.forName("utf-8")
+
+/**
+ * An util class which handles file I/O
+ */
+object FileUtil {
+    /**
+     * delete folder and its content recursively.
+     * @param targetDirectory directory to be deleted
+     */
+    fun deleteFolderRecursively(targetDirectory: File) {
+        try {
+            Files.walkFileTree(targetDirectory.toPath(), object : SimpleFileVisitor<Path>() {
+                override fun visitFile(file: Path, attrs: BasicFileAttributes): FileVisitResult {
+                    Files.delete(file)
+                    return FileVisitResult.CONTINUE
+                }
+
+                override fun postVisitDirectory(dir: Path, exc: IOException?): FileVisitResult {
+                    if (exc != null) {
+                        throw exc
+                    }
+                    Files.delete(dir)
+                    return FileVisitResult.CONTINUE
+                }
+            })
+        } catch (e: IOException) {
+            println("Error occured while browsing files under " + targetDirectory.toString() + ": " + e.toString())
+        }
+
+    }
+
+    /**
+     * Write json data to the given target file
+     * @param targetFile target file to which json data should be written.
+     *                   File may not exist at the time of method invocation, but should not be a directory.
+     *
+     * @param jsonObject a source json object
+     */
+    fun writeJSON(targetFile: File, jsonObject: JsonObject) {
+        if (!targetFile.exists()) {
+            val parent = targetFile.parentFile
+            if (!parent.exists() && !parent.mkdirs()) {
+                throw IOException("Failed to create target directory!")
+            }
+        }
+
+        Files.newOutputStream(targetFile.toPath()).use { oStream ->
+            val writeData = jsonObject.toString().toByteArray(fileFormat)
+            oStream.write(writeData)
+        }
+    }
+
+    /**
+     * Read json data from the given target file
+     */
+    fun readJSON(targetFile: File): JsonObject {
+        Files.newBufferedReader(targetFile.toPath(), fileFormat)
+                .use { reader -> return JsonParser().parse(reader).asJsonObject }
+    }
+
+    /**
+     * Get the name of the file with it's extension removed.
+     * @param file target file
+     *
+     * @return file name without the extension
+     */
+    fun getFileBaseName(file: File): String {
+        val fileName = file.name
+        val lastIndexOfDot = fileName.lastIndexOf(".")
+        if (lastIndexOfDot == 0) {
+            return fileName
+        }
+        return fileName.substring(0, lastIndexOfDot)
+    }
+}
