ba74cffad2e0f53df727ccb571a1d7a70e5c3a58
Kory33 <6561358+kory33@users.noreply.github.com>
2017-11-08 10:00:45 +0900

    change permission root node / add plugin.yml

--- src/main/kotlin/com/github/kory33/usercommandregistry/UCRPermissions.kt
+++ src/main/kotlin/com/github/kory33/usercommandregistry/UCRPermissions.kt
@@ -1,7 +1,7 @@
 package com.github.kory33.usercommandregistry
 
 object UCRPermissions {
-    private const val ROOT = "usercommandregistry"
+    private const val ROOT = "ucr"
 
     const val RELOAD = "$ROOT.reload"
     const val EXEC = "$ROOT.exec"
