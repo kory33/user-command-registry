8c797700ab2e63e3b5bf5bd68aaa1bfdce2ec584
Kory33 <6561358+kory33@users.noreply.github.com>
2017-11-09 11:20:41 +0900

    add line break to SimpleMessageUI by default

--- src/main/kotlin/com/github/kory33/usercommandregistry/ui/SimpleMessageUI.kt
+++ src/main/kotlin/com/github/kory33/usercommandregistry/ui/SimpleMessageUI.kt
@@ -4,6 +4,7 @@ import com.github.kory33.chatgui.tellraw.MessagePartsList
 import com.github.kory33.usercommandregistry.ui.template.ChatInterfaceTemplate
 import com.github.kory33.usercommandregistry.util.config.LocaleConfig
 
-class SimpleMessageUI(private val message: String, locale: LocaleConfig) : ChatInterfaceTemplate(locale) {
-    override fun constructBodyMassage() = MessagePartsList(message)
+class SimpleMessageUI(private val message: String, locale: LocaleConfig, private val withLineBreak: Boolean = true)
+    : ChatInterfaceTemplate(locale) {
+    override fun constructBodyMassage() = MessagePartsList(message + (if (withLineBreak) "\n" else ""))
 }
