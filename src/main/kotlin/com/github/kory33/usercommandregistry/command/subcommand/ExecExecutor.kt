d0169c0e8fc3847c167f38d2c9c8dcc4ad31fb91
Kory33 <6561358+kory33@users.noreply.github.com>
2017-11-09 13:53:32 +0900

    implement execution loop guard

--- src/main/kotlin/com/github/kory33/usercommandregistry/command/subcommand/ExecExecutor.kt
+++ src/main/kotlin/com/github/kory33/usercommandregistry/command/subcommand/ExecExecutor.kt
@@ -8,6 +8,20 @@ import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 
 class ExecExecutor(private val plugin: UserCommandRegistry) : SubCommandExecutor {
+    /**
+     * The server tends to crash itself in the following situation:
+     *  - There is an alias of `crash` -> `ucr exec crash`
+     *  - Player executes `ucr exec crash`
+     *
+     * In which case, this executor needs to know if the command has been executed within the same command execution.
+     * Therefore, it first checks if the player has already executed the command,
+     * if not, put the alias into this set and then execute the alias, and then empty the execution stack.
+     *
+     * This kind of mechanism is only valid in a single threaded execution, therefore it fails in any environment
+     * which makes use of multithreaded command executor.
+     */
+    private val executingAlias = HashSet<String>()
+
     override val helpString
             get() = plugin.locale["help.sub_command.exec"]
 
@@ -29,20 +43,30 @@ class ExecExecutor(private val plugin: UserCommandRegistry) : SubCommandExecutor
             return false
         }
 
-        val registry = plugin.commandRegistryManager!!.getLoadedPlayerData(player.uniqueId)
+        val registry = plugin.commandRegistryManager?.getLoadedPlayerData(player.uniqueId)
         if (registry == null) {
             SimpleMessageUI(locale["ui.message.data_loading"], locale).send(player)
             return true
         }
 
-        val aliasTarget = registry.getAlias(args.first())
+        val alias = args.first()
+        val aliasTarget = registry.getAlias(alias)
         if (aliasTarget == null) {
             SimpleMessageUI(locale["ui.message.exec.target_missing"], locale).send(player)
             return true
         }
 
+        if (executingAlias.contains(alias)) {
+            SimpleMessageUI(locale.getFormatted("ui.message.exec.recursion_detected", alias), locale).send(player)
+            return true
+        }
+
+        executingAlias.add(alias)
+
         player.sendMessage(locale.getFormatted("ui.message.exec.performing", aliasTarget.targetCommand))
         player.performCommand(aliasTarget.targetCommand)
+
+        executingAlias.remove(alias)
         return true
     }
 }
